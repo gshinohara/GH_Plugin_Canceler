223ce3e1921ff597a00aba36a7c7651f43f1847d
Gaku Shinohara <gaku.shinohara@vicc.jp>
2024-10-06 20:46:05 +0900

    Add canceler dialog

---
+++ GH_Plugin_Canceler/CancelSelector.cs
@@ -0,0 +1,47 @@
+﻿using Eto.Forms;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace GH_Plugin_Canceler
+{
+    internal class CancelSelector : Dialog<ResultOfCanceler>
+    {
+        private Label m_LabelCounting = new Label();
+
+        public CancelSelector(Assembly assembly)
+        {
+            Width = 250;
+            Padding = 5;
+            AutoSize = true;
+            Topmost = true;
+
+            DynamicLayout layout = new DynamicLayout
+            {
+                Padding = 15,
+                Spacing = new Eto.Drawing.Size(5, 10),
+            };
+            Content = layout;
+
+            layout.AddSeparateRow(new Label { Text = $"Do you want to load {assembly.GetName().Name} plug-in?", Wrap = WrapMode.Word });
+            layout.AddSeparateRow(m_LabelCounting);
+
+            Button button_Yes = new Button((s, e) => Close(ResultOfCanceler.NoProblem)) { Text = "Yes", Width = 90 };
+            Button button_No = new Button((s, e) => Close(ResultOfCanceler.ShouldBeCanceled)) { Text = "No", Width = 90 };
+            layout.AddSeparateRow(button_Yes, null, button_No);
+        }
+
+        protected override async void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+
+            int count = 20;
+            for (int i = 0; i < count; i++)
+            {
+                m_LabelCounting.Text = $"If you wait for {count - i} seconds, the plug-in is loaded.";
+                await Task.Delay(1000);
+            }
+            Close(ResultOfCanceler.NoProblem);
+        }
+    }
+}
---
+++ GH_Plugin_Canceler/Implementer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GH_Plugin_Canceler
+{
+    /// <summary>
+    /// The result of canceler.
+    /// </summary>
+    public enum ResultOfCanceler
+    {
+        /// <summary>
+        /// The method of PriorityLoad() should return GH_LoadingInstruction.Abort.
+        /// </summary>
+        ShouldBeCanceled,
+        /// <summary>
+        /// The method of PriorityLoad() should not return anything.
+        /// </summary>
+        NoProblem,
+    }
+
+    public static class Implementer
+    {
+        public static ResultOfCanceler Canceler<TAssemblyPriority>(this TAssemblyPriority priority)
+            where TAssemblyPriority : Grasshopper.Kernel.GH_AssemblyPriority
+        {
+            return new CancelSelector(typeof(TAssemblyPriority).Assembly).ShowModal(Rhino.UI.RhinoEtoApp.MainWindow);
+        } 
+    }
+}
